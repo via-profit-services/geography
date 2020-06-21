bea74bf5049ef1c5754530ca17fa99f64f62e5be
Vasily Novosad <delhsmail@gmail.com>
2020-06-21 22:17:04 +0500

    Build

--- dist/schemas/geography/types.d.ts
+++ dist/schemas/geography/types.d.ts
@@ -2,27 +2,48 @@ import { IContext } from '@via-profit-services/core';
 export declare type Context = Pick<IContext, 'knex' | 'timezone' | 'logger'>;
 export interface ICountry {
     id: string;
-    name: string;
+    en: string;
+    ru: string;
     iso3: string;
     iso2: string;
     phoneCode: string;
     currency: string;
-    capital?: string;
+    capital: {
+        id: string;
+    } | null;
 }
+export declare type ICountryPlaneInput = Omit<ICountry, 'capital'> & {
+    capital: string;
+};
 export interface IState {
     id: string;
-    name: string;
-    country: string;
+    en: string;
+    ru: string;
+    country: {
+        id: string;
+    };
     countryCode: string;
     stateCode: string;
 }
+export declare type IStatePlaneInput = Omit<IState, 'country'> & {
+    country: string;
+};
 export interface ICity {
     id: string;
-    name: string;
-    country: string;
+    en: string;
+    ru: string;
+    country: {
+        id: string;
+    };
     countryCode: string;
-    state: string;
+    state: {
+        id: string;
+    };
     stateCode: string;
     latitude: string;
     longitude: string;
 }
+export declare type ICityPlaneInput = Omit<ICity, 'country' | 'state'> & {
+    country: string;
+    state: string;
+};
