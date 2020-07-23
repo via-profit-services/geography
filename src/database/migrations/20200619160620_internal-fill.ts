5e9014d6b1473a7dd941600323e99025d277c823
Vasily Novosad <delhsmail@gmail.com>
2020-07-24 00:30:44 +0500

    timezone migrations

--- src/database/migrations/20200619160620_internal-fill.ts
+++ src/database/migrations/20200619160620_internal-fill.ts
@@ -1,30 +1,10 @@
 /* eslint-disable import/no-extraneous-dependencies */
 import * as Knex from 'knex';
-import * as ae from '../../countries/AE';
-import * as af from '../../countries/AF';
-
-const list = [ae, af];
 
 export async function up(knex: Knex): Promise<any> {
-  return knex.raw(`
-    delete from "geographyCities";
-    delete from "geographyStates";
-    delete from "geographyCountries";
-  `)
-    .then(() => {
-      return list.reduce(async (prev, { countries, states, cities }) => {
-        await prev;
-        await knex('geographyCountries').insert(countries);
-        await knex('geographyStates').insert(states);
-        await knex('geographyCities').insert(cities);
-      }, Promise.resolve());
-    });
+  return knex.raw('');
 }
 
 export async function down(knex: Knex): Promise<any> {
-  return knex.raw(`
-    delete from "geographyCities";
-    delete from "geographyStates";
-    delete from "geographyCountries";
-  `);
+  return knex.raw('');
 }
