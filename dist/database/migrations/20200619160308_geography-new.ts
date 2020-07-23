c73d01820e9026eb35c64ff0313a527f6bc9e0ed
Vasily Novosad <delhsmail@gmail.com>
2020-07-24 01:10:39 +0500

    Build

--- dist/database/migrations/20200619160308_geography-new.ts
+++ dist/database/migrations/20200619160308_geography-new.ts
@@ -16,10 +16,11 @@ export async function up(knex: Knex): Promise<any> {
     alter table "geographyStates" drop column "ru";
     alter table "geographyCountries" drop column "ru";
     
+    alter table "geographyCities" add column "timezone" varchar(60) NOT NULL;
     alter table "geographyCities" add column "en" varchar(100) NOT NULL;
     alter table "geographyStates" add column "en" varchar(100) NOT NULL;
     alter table "geographyCountries" add column "en" varchar(100) NOT NULL;
-    
+
     alter table "geographyCities" add column "ru" varchar(100) NOT NULL;
     alter table "geographyStates" add column "ru" varchar(100) NOT NULL;
     alter table "geographyCountries" add column "ru" varchar(100) NOT NULL;
@@ -34,10 +35,11 @@ export async function down(knex: Knex): Promise<any> {
     truncate table "geographyStates" cascade;
     truncate table "geographyCountries" cascade;
     
+    alter table "geographyCities" drop column "timezone" cascade;
     alter table "geographyCities" drop column "en";
     alter table "geographyStates" drop column "en";
     alter table "geographyCountries" drop column "en";
-
+    
     alter table "geographyCities" add column "name" varchar(100) NOT NULL;
     alter table "geographyStates" add column "name" varchar(100) NOT NULL;
     alter table "geographyCountries" add column "name" varchar(100) NOT NULL;
